9764ccb3c1dd75ef67b835ea183ce6b4f76d9d9e
Carlos Guttemberg <guttembergcarlos@gmail.com>
2024-04-09 21:52:32 -0300

    first commit

---
+++ src/main/java/com/guttembergcarlos/algorithm_solver/AlgorithmSolverApplication.java
@@ -0,0 +1,13 @@
+package com.guttembergcarlos.algorithm_solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmSolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmSolverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guttembergcarlos/algorithm_solver/AlgorithmSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guttembergcarlos.algorithm_solver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmSolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
